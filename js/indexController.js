e29f110da8ed964ad75770689f89bd472f337b4a
ghadir32 <ghadirfwzzz@gmail.com>
2023-01-31 13:40:22 +0200

    update

--- js/indexController.js
+++ js/indexController.js
@@ -1,13 +1,15 @@
 //my routes
 portfolio.controller('homeController', ['$scope', function($scope){
 
+ 
     $scope.val = false;
 
     $scope.enable = function(){
+        
        if( $scope.val == false){
              return true;
        }else{
-                return false;
+             return false;
        }
  
 
